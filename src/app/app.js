9f1f2edbe91f67537ddb040cc25d9ed55f80e7b7
Hyunsang <asunhs@gmail.com>
2016-04-13 21:14:45 +0900

    sandbaxApp

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-module.exports = angular.module('packApp', [
+module.exports = angular.module('sandbaxApp', [
     'templates-html'
 ]).config(/* @ngInject */ function ($compileProvider, $httpProvider) {
     $compileProvider.debugInfoEnabled(false);
