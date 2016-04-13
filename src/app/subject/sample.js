a63e7afcb400eb2ec01282fc8649c7c777d73def
Hyunsang <asunhs@gmail.com>
2016-04-14 08:26:13 +0900

    babel update

--- src/app/subject/sample.js
+++ src/app/subject/sample.js
@@ -4,6 +4,12 @@ function SampleCtrl ($scope) {
     $scope.name = "Foods";
 }
 
+function hello(fn) {
+    fn("hello");
+}
+
+hello(str => console.log(str));
+
 module.exports = require('app').directive('packSample', /* @ngInject */ function () {
     return {
         templateUrl: "subject/sample.tpl.html",
